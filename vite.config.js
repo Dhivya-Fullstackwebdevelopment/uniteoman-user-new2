17bef5d79fcd6d9191206688cebdd4d8a055cf93
Dhivya-Fullstackwebdevelopment <divya.p9842@gmail.com>
2026-07-31 16:23:11 +0530

    Update viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: { '@': path.resolve(__dirname, './src') }
-//   },
-//   server: {
-//     host: true,
-//     allowedHosts: ['uniteoman.com'],
-//     proxy: {
-//       '/api': { target: 'https://api.uniteoman.com/', changeOrigin: true },
-//       '/uploads': { target: 'https://api.uniteoman.com/', changeOrigin: true }
-//     }
-//   }
-
-// })
-
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
+  },
+  server: {
+    host: true,
+    allowedHosts: ['uniteoman.com'],
+    proxy: {
+      '/api': { target: 'https://api.uniteoman.com/', changeOrigin: true },
+      '/uploads': { target: 'https://api.uniteoman.com/', changeOrigin: true }
+    }
+  }
+
 })
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
